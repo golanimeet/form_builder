0c6504144b545b6439d0e8850d239490b15a9960
golanimeet <golanimeet7@gmail.com>
2025-02-19 11:53:15 +0530

    template file default chnages

--- form_builder_frontend/src/Pages/Templates.jsx
+++ form_builder_frontend/src/Pages/Templates.jsx
@@ -5,8 +5,6 @@ export default function Templates() {
     <>
       <div>Templates</div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, in.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, in.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, in.</p>
     </>
   )
 }
